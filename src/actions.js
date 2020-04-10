f1c93e2c99526b7e580935eb4d1ce21b54554cac
Kacper Szot <kacper@localhost.localdomain>
2020-04-10 16:33:56 +0200

    create action creators

--- src/actions.js
+++ src/actions.js
@@ -5,3 +5,19 @@
 export const ADD_TODO = 'ADD_TODO'
 export const TOGGLE_TODO = 'TOGGLE_TODO'
 export const REMOVE_TODO = 'REMOVE_TODO'
+
+/**
+ *action creators
+ */
+
+export function addTodo(text) {
+    return { type: ADD_TODO, text }
+}
+
+export function toggleTodo(index) {
+    return { type: TOGGLE_TODO, index }
+}
+
+export function removeTodo(index) {
+    return { type: REMOVE_TODO, index }
+}

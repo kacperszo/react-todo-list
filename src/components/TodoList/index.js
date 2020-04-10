dc84a2f6b1704fa051fd9cf0ce463a30aa79616f
Kacper Szot <kacperszot1@gmail.com>
2020-04-10 19:47:57 +0200

    add new todo modal component and new todo logic

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { addTodo, removeTodo, toggleTodo } from '../../actions'
 import { connect } from "react-redux";
 import styles from "./style.module.scss";
 import TodoItem from "../TodoItem"
 import NewTodoButton from "../NewTodoButton"
+import NewTodoModal from "../NewTodoModal"
 const mapStateToProps = state => state
 
 const mapDispatchToProps = {
@@ -13,6 +14,18 @@ const mapDispatchToProps = {
 }
 
 function TodoList(props) {
+    const [modalOpen, setModalOpen] = useState(false)
+
+    const toggleModal = () => setModalOpen(!modalOpen)
+    const createNewTodo = (text) => {
+
+        if (text === '') {
+            toggleModal()
+        } else {
+            props.addTodo(text)
+            toggleModal()
+        }
+    }
 
     return (
         <div className={styles.container}>
@@ -29,8 +42,16 @@ function TodoList(props) {
                 }
             </div>
             <div className={styles.buttonWrapper}>
-                <NewTodoButton />
+                <NewTodoButton onClick={toggleModal} />
             </div>
+            {
+                modalOpen ?
+                    <div className={styles.modalWrapper}>
+                        <NewTodoModal onSubmit={createNewTodo} />
+                    </div>
+                    :
+                    null
+            }
         </div>
     )
 }
